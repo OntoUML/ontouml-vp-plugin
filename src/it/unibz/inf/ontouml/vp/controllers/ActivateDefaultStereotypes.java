46f829e9b085d80f2a8217b7008e46dbf65dcfdf
Viola <victorviola86@gmail.com>
2019-12-10 18:45:14 +0000

    improve the activation of the default stereotypes
    when activate the default stereotype, removed the stereotypes that are  already in the classes.

--- src/it/unibz/inf/ontouml/vp/controllers/ActivateDefaultStereotypes.java
+++ src/it/unibz/inf/ontouml/vp/controllers/ActivateDefaultStereotypes.java
@@ -2,6 +2,8 @@ package it.unibz.inf.ontouml.vp.controllers;
 
 import com.vp.plugin.action.VPAction;
 import com.vp.plugin.action.VPActionController;
+import com.vp.plugin.model.factory.IModelElementFactory;
+
 import it.unibz.inf.ontouml.vp.OntoUMLPluginForVP;
 import it.unibz.inf.ontouml.vp.utils.StereotypeUtils;
 
@@ -9,7 +11,9 @@ public class ActivateDefaultStereotypes implements VPActionController {
 	
 	@Override
 	public void performAction(VPAction arg0) {
-	
+		
+		StereotypeUtils.removeAllModelSteryotypesButOntoUML(IModelElementFactory.MODEL_TYPE_CLASS);
+		StereotypeUtils.removeAllModelSteryotypesButOntoUML(IModelElementFactory.MODEL_TYPE_ASSOCIATION);
 		StereotypeUtils.setDefaultStereotypes(OntoUMLPluginForVP.allModelElements);
 		
 	}
