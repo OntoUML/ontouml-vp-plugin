8903b1427444bd4f83d92985db659d4f50b89e2e
Viola <victorviola86@gmail.com>
2020-01-19 16:51:02 +0000

    Removed XML Export Window.

--- src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelExportAction.java
+++ src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelExportAction.java
@@ -1,6 +1,7 @@
 package it.unibz.inf.ontouml.vp.controllers;
 
-import java.io.File;
+import java.awt.FileDialog;
+import java.awt.Frame;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,7 +9,6 @@ import java.nio.file.Paths;
 import com.vp.plugin.ApplicationManager;
 import com.vp.plugin.action.VPAction;
 import com.vp.plugin.action.VPActionController;
-import com.vp.plugin.model.IProject;
 
 import it.unibz.inf.ontouml.vp.model.ModelElement;
 import it.unibz.inf.ontouml.vp.utils.Configurations;
@@ -17,8 +17,8 @@ import it.unibz.inf.ontouml.vp.utils.ViewUtils;
 
 /**
  * 
- * Implementation toolbar button action responsible for exporting OntoUML model in both XMI (according
- * UML's standard) and JSON (according to OntoUML Schema).
+ * Implementation toolbar button action responsible for exporting OntoUML model
+ * in  JSON (according to OntoUML Schema).
  * 
  * @author Claudenir Fonseca
  *
@@ -27,7 +27,7 @@ public class ModelExportAction implements VPActionController {
 
 	/**
 	 * 
-	 * Performs model export in XMI and JSON formats to user defined folder.
+	 * Performs model export in JSON format.
 	 * 
 	 * @param action
 	 * 
@@ -35,26 +35,38 @@ public class ModelExportAction implements VPActionController {
 	@Override
 	public void performAction(VPAction action) {
 		final ProjectConfigurations configurations = Configurations.getInstance().getProjectConfigurations();
-		
-		final IProject project = ApplicationManager.instance().getProjectManager().getProject();
+
 		final String jsonModel = ModelElement.generateModel(true);
-		final File outputXMLFolder = new File(configurations.getExportFolderPath());
-		
-		try {
-			ApplicationManager.instance().getModelConvertionManager().showExportXMLDialog(outputXMLFolder);
-            Files.write(Paths.get(configurations.getExportFolderPath(), project.getName() + ".json"), 
-            		jsonModel.getBytes());
-            ViewUtils.log("JSON model successfully exported.", ViewUtils.SCOPE_PLUGIN);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+		FileDialog fd = new FileDialog((Frame) ApplicationManager.instance().getViewManager().getRootFrame(),
+				"Choose destination", FileDialog.SAVE);
+
+		fd.setDirectory(configurations.getExportFolderPath());
+		fd.setFile("*.json");
+		fd.setVisible(true);
+
+		String fileDirectory = fd.getDirectory();
+		String filename = fd.getFile() + ".json";
+
+		if (fileDirectory != null) {
+			try {
+				Files.write(Paths.get(fileDirectory, filename), jsonModel.getBytes());
+				configurations.setExportFolderPath(fileDirectory);
+			} catch (IOException e) {
+				ViewUtils.log(
+						"Export Failed. Please submit your Visual Paradigm's log and the time of the error our developers.",
+						ViewUtils.SCOPE_PLUGIN);
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 	/**
 	 * Called when the menu containing the button is accessed allowing for action
 	 * manipulation, such as enable/disable or selecting the button.
 	 * 
-	 *  OBS: DOES NOT apply to this class.
+	 * OBS: DOES NOT apply to this class.
 	 */
 	@Override
 	public void update(VPAction action) {
--- src/main/java/it/unibz/inf/ontouml/vp/views/ConfigurationsView.java
+++ src/main/java/it/unibz/inf/ontouml/vp/views/ConfigurationsView.java
@@ -7,17 +7,13 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import com.vp.plugin.ApplicationManager;
 import com.vp.plugin.view.IDialog;
 
 import it.unibz.inf.ontouml.vp.utils.Configurations;
