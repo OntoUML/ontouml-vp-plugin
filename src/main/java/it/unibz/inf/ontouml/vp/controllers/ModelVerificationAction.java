1f844e8a5517afce7ab18a97eb08e7f430b8fc28
Victor Viola <victor.viola@farfetch.com>
2020-02-06 21:13:11 +0000

    ready to be merged

--- src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelVerificationAction.java
+++ src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelVerificationAction.java
@@ -34,16 +34,7 @@ public class ModelVerificationAction implements VPActionController {
 	@Override
 	public void performAction(VPAction action) {
 		
-		final String modelo = ModelElement.generateModel(true);
-		System.out.println(modelo);
-
-		// Send json model to clipboard
-		Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
-		StringSelection sl = new StringSelection(modelo);
-		c.setContents(sl, sl);
-		System.out.println("Generated JSON copied to clipboard ;)");
-		
-	/*	ExecutorService executor = Executors.newFixedThreadPool(10);
+		ExecutorService executor = Executors.newFixedThreadPool(10);
 		executor.execute(new Runnable() {
 
 			@Override
@@ -67,7 +58,7 @@ public class ModelVerificationAction implements VPActionController {
 				}
 			}
 
-		});*/
+		});
 	}
 
 	/**
