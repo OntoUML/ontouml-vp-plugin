54084a6216f0759aef2b1799d42e88b53fe523b8
Victor Viola <victor.viola@farfetch.com>
2020-02-12 22:54:23 +0000

    export json
    Changed view to File Dialog
    Now suggests the same name of the last save

--- src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelExportAction.java
+++ src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelExportAction.java
@@ -8,20 +8,17 @@ import it.unibz.inf.ontouml.vp.utils.Configurations;
 import it.unibz.inf.ontouml.vp.utils.ProjectConfigurations;
 import it.unibz.inf.ontouml.vp.utils.ViewUtils;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.awt.*;
 import java.io.IOException;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * 
  * Implementation toolbar button action responsible for exporting OntoUML model
- * in JSON (according to OntoUML Schema).
+ * in  JSON (according to OntoUML Schema).
  * 
  * @author Claudenir Fonseca
- * @author Victor Viola
  *
  */
 public class ModelExportAction implements VPActionController {
@@ -33,60 +30,55 @@ public class ModelExportAction implements VPActionController {
 	 * @param action
 	 * 
 	 */
-	@SuppressWarnings("resource")
 	@Override
 	public void performAction(VPAction action) {
-		final ProjectConfigurations configurations = Configurations.getInstance().getProjectConfigurations();
+		final ProjectConfigurations projectConfigurations = Configurations.getInstance().getProjectConfigurations();
+		final Configurations configs = Configurations.getInstance();
 
-		@SuppressWarnings("serial")
-		JFileChooser fileChooser = new JFileChooser() {
-			@Override
-			public void approveSelection() {
-				File f = getSelectedFile();
-				if (f.exists() && getDialogType() == SAVE_DIALOG) {
-					int result = JOptionPane.showConfirmDialog(this, "File already exists.\nDo you want to replace it?", "Confirm Save As", JOptionPane.YES_NO_CANCEL_OPTION);
-					switch (result) {
-					case JOptionPane.YES_OPTION:
-						super.approveSelection();
-						return;
-					case JOptionPane.NO_OPTION:
-						return;
-					case JOptionPane.CLOSED_OPTION:
-						return;
-					case JOptionPane.CANCEL_OPTION:
-						cancelSelection();
-						return;
-					}
-				}
-				super.approveSelection();
-			}
-		};
+		FileDialog fd = new FileDialog((Frame) ApplicationManager.instance().getViewManager().getRootFrame(),
+				"Choose destination", FileDialog.SAVE);
+		
+		
+		String suggestedFilename;
+		fd.setDirectory(projectConfigurations.getExportFolderPath());
+		
+		if(projectConfigurations.getExportFilename().length() == 0){
+			String projectName = ApplicationManager.instance().getProjectManager().getProject().getName();		
+			suggestedFilename = projectName;
+			fd.setFile(suggestedFilename);
+		}else{
+			suggestedFilename = projectConfigurations.getExportFilename();
+			fd.setFile(suggestedFilename);
+		}
+		
+		fd.setVisible(true);
 
-		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("JSON File", "json"));
-		fileChooser.setAcceptAllFileFilterUsed(true);
+		String fileDirectory = fd.getDirectory();
 
-		fileChooser.setCurrentDirectory(new File(configurations.getExportFolderPath()));
-		fileChooser.setSelectedFile(new File(ApplicationManager.instance().getProjectManager().getProject().getName() + ".json"));
+		String fileName = fd.getFile();
+		if(!fileName.endsWith(".json"))
+			fileName+=".json";
 
-		int result = fileChooser.showSaveDialog(ApplicationManager.instance().getViewManager().getRootFrame());
-
-		if (result == JFileChooser.APPROVE_OPTION) {
+		if (fileDirectory != null) {
 			try {
-				FileWriter fw = new FileWriter(fileChooser.getSelectedFile().getAbsolutePath() + ".json");
 				final String jsonModel = ModelElement.generateModel(true);
-				fw.write(jsonModel);
-				configurations.setExportFolderPath(fileChooser.getCurrentDirectory().toString());
+				Files.write(Paths.get(fileDirectory, fileName), jsonModel.getBytes());
+				projectConfigurations.setExportFolderPath(fileDirectory);
+				projectConfigurations.setExportFilename(fileName);
+				configs.save();
 			} catch (IOException e) {
-				ViewUtils.log("Export Failed. Please submit your Visual Paradigm's log and the time of the error our developers.", ViewUtils.SCOPE_PLUGIN);
+				ViewUtils.log(
+						"Export Failed. Please submit your Visual Paradigm's log and the time of the error our developers.",
+						ViewUtils.SCOPE_PLUGIN);
 				e.printStackTrace();
 			}
 		}
+
 	}
 
 	/**
-	 * Called when the menu containing the button is accessed allowing for
-	 * action manipulation, such as enable/disable or selecting the button.
+	 * Called when the menu containing the button is accessed allowing for action
+	 * manipulation, such as enable/disable or selecting the button.
 	 * 
 	 * OBS: DOES NOT apply to this class.
 	 */
