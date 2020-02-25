4e86ee7bd273fdecf7fa0c0580856c92befb34c5
Victor Viola <victor.viola@farfetch.com>
2020-02-25 20:01:40 +0000

    fixed coloring to match mixin pattern

--- src/main/java/it/unibz/inf/ontouml/vp/utils/SmartColoring.java
+++ src/main/java/it/unibz/inf/ontouml/vp/utils/SmartColoring.java
@@ -1,6 +1,10 @@
 package it.unibz.inf.ontouml.vp.utils;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.vp.plugin.ApplicationManager;
 import com.vp.plugin.diagram.IDiagramElement;
@@ -40,10 +44,11 @@ public class SmartColoring {
 
 	public static final Color COLOR_NON_SORTAL = new Color(224, 224, 224);
 
+	private static final ArrayList<String> identityProviderList = new ArrayList<>(Arrays.asList(StereotypeUtils.STR_KIND, StereotypeUtils.STR_COLLECTIVE, StereotypeUtils.STR_QUANTITY, StereotypeUtils.STR_MODE, StereotypeUtils.STR_RELATOR, StereotypeUtils.STR_QUALITY, StereotypeUtils.STR_TYPE));
+
 	/**
 	 * 
-	 * Paints the assigned diagram element with the assigned color. No effect
-	 * whenever auto-coloring is disabled or color is <code>null</code>.
+	 * Paints the assigned diagram element with the assigned color. No effect whenever auto-coloring is disabled or color is <code>null</code>.
 	 * 
 	 * @param diagramElement
 	 * @param color
@@ -122,14 +127,14 @@ public class SmartColoring {
 
 	/**
 	 * 
-	 * Returns first sortal color occurring on one of the generalizations of this
-	 * class. If generalization with such color is found, returns <code>null</code>.
+	 * Returns first sortal color occurring on one of the generalizations of this class. If generalization with such color is found, returns <code>null</code>.
 	 * 
 	 * @param context
 	 * 
 	 */
 	public static Color inferColorBasedSuper(IClass _class) {
 		final ISimpleRelationship[] specializations = _class.toToRelationshipArray();
+		ArrayList<Color> superColors = new ArrayList<Color>();
 
 		if (specializations == null)
 			return COLOR_NON_SORTAL;
@@ -142,6 +147,9 @@ public class SmartColoring {
 			final IModelElement superClass = specializations[i].getFrom();
 			final IDiagramElement[] superDiagramElements = superClass.getDiagramElements();
 
+			if (superClass.getModelType().equals(IModelElementFactory.MODEL_TYPE_CLASS) && !hasIdentityProvider(((IClass) superClass)))
+				continue;
+
 			for (int j = 0; j < superDiagramElements.length; j++) {
 				if (!(superDiagramElements[j] instanceof IShapeUIModel)) {
 					continue;
@@ -149,14 +157,22 @@ public class SmartColoring {
 
 				final Color superColor = ((IShapeUIModel) superDiagramElements[j]).getFillColor().getColor1();
 
-				return getSortalColor(superColor);
+				if (!getSortalColor(superColor).equals(COLOR_NON_SORTAL) && !superColors.contains(getSortalColor(superColor))) {
+					superColors.add(getSortalColor(superColor));
+				}
 			}
 		}
-		return COLOR_NON_SORTAL;
+		if (superColors.size() == 1) {
+			return superColors.get(0);
+		} else {
+			return COLOR_NON_SORTAL;
+		}
 	}
 
 	public static Color inferColorBasedSpecialization(IClass _class) {
 		final ISimpleRelationship[] specializations = _class.toFromRelationshipArray();
+		ArrayList<Color> specializedColors = new ArrayList<Color>();
+		// specializedColors.add(COLOR_NON_SORTAL);
 
 		if (specializations == null)
 			return COLOR_NON_SORTAL;
@@ -174,14 +190,21 @@ public class SmartColoring {
 					continue;
 				}
 
-				final Color specializedColor = ((IShapeUIModel) specializedDiagramElements[j]).getFillColor()
-						.getColor1();
+				final Color specializedColor = ((IShapeUIModel) specializedDiagramElements[j]).getFillColor().getColor1();
 
-				return getSortalColor(specializedColor);
-
+				if (!specializedColors.contains(getSortalColor(specializedColor))) {
+					specializedColors.add(getSortalColor(specializedColor));
+				}
 			}
 		}
-		return COLOR_NON_SORTAL;
+
+		List<Color> listWithoutDuplicates = specializedColors.stream().distinct().collect(Collectors.toList());
+
+		if (listWithoutDuplicates.size() > 1) {
+			return COLOR_NON_SORTAL;
+		} else {
+			return listWithoutDuplicates.get(0);
+		}
 	}
 
 	private static Color getSortalColor(Color color) {
@@ -203,6 +226,17 @@ public class SmartColoring {
 		}
 		return COLOR_NON_SORTAL;
 
+	}
+
+	private static boolean hasIdentityProvider(IClass _class) {
+		final String[] stereotypes = _class.toStereotypeArray();
+
+		for (int i = 0; stereotypes != null && i < stereotypes.length; i++) {
+			if (identityProviderList.contains(stereotypes[i]))
+				return true;
+		}
+
+		return false;
 	}
 
 	public static void smartPaint() {
