404ccf72bc09576b47a5a2ddf5c9a6bcc62c3546
Viola <victorviola86@gmail.com>
2020-03-01 10:48:24 +0000

    Added a warning when the user clicks on smart paint and the auto coloring is set to false.

--- src/main/java/it/unibz/inf/ontouml/vp/controllers/PaintModelAction.java
+++ src/main/java/it/unibz/inf/ontouml/vp/controllers/PaintModelAction.java
@@ -1,14 +1,25 @@
 package it.unibz.inf.ontouml.vp.controllers;
 
+import com.vp.plugin.ApplicationManager;
+import com.vp.plugin.ViewManager;
 import com.vp.plugin.action.VPAction;
 import com.vp.plugin.action.VPActionController;
 
+import it.unibz.inf.ontouml.vp.utils.Configurations;
 import it.unibz.inf.ontouml.vp.utils.SmartColoring;
 
 public class PaintModelAction implements VPActionController {
+	
+	ViewManager _viewManager = ApplicationManager.instance().getViewManager();	
 
 	@Override
 	public void performAction(VPAction arg0) {
+		
+		if (!Configurations.getInstance().getProjectConfigurations().isAutomaticColoringEnabled()) {
+			_viewManager.showMessage("Smart Paint is NOT enabled! Please activate it on the plugin settings.");		
+			return;
+		}
+		
 		SmartColoring.smartPaint();
 	}
 
