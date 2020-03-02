77b596a5bf318472c97ca10e0f8cfbc48ea3c456
Viola <victorviola86@gmail.com>
2020-03-02 15:45:06 +0000

    added dialog box when user clicks in smart paint

--- src/main/java/it/unibz/inf/ontouml/vp/controllers/PaintModelAction.java
+++ src/main/java/it/unibz/inf/ontouml/vp/controllers/PaintModelAction.java
@@ -1,22 +1,24 @@
 package it.unibz.inf.ontouml.vp.controllers;
 
-import com.vp.plugin.ApplicationManager;
-import com.vp.plugin.ViewManager;
 import com.vp.plugin.action.VPAction;
 import com.vp.plugin.action.VPActionController;
 
 import it.unibz.inf.ontouml.vp.utils.Configurations;
 import it.unibz.inf.ontouml.vp.utils.SmartColoring;
+import it.unibz.inf.ontouml.vp.utils.ViewUtils;
 
 public class PaintModelAction implements VPActionController {
-	
-	ViewManager _viewManager = ApplicationManager.instance().getViewManager();	
 
 	@Override
 	public void performAction(VPAction arg0) {
 		
 		if (!Configurations.getInstance().getProjectConfigurations().isAutomaticColoringEnabled()) {
-			_viewManager.showMessage("Smart Paint is NOT enabled! Please activate it on the plugin settings.");		
+			
+			if(ViewUtils.enableSmartPaintDialog()) {
+				Configurations.getInstance().getProjectConfigurations().setAutomaticColoringEnabled(true);
+				SmartColoring.smartPaint();
+			}
+			
 			return;
 		}
 		
--- src/main/java/it/unibz/inf/ontouml/vp/utils/ViewUtils.java
+++ src/main/java/it/unibz/inf/ontouml/vp/utils/ViewUtils.java
@@ -1,6 +1,10 @@
 package it.unibz.inf.ontouml.vp.utils;
 
+import java.io.File;
 import java.sql.Timestamp;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -11,66 +15,85 @@ import com.vp.plugin.ApplicationManager;
 
 /**
  * 
- * Class responsible for facilitating display of messages on Visual Paradigm's log.
+ * Class responsible for facilitating display of messages on Visual Paradigm's
+ * log.
  * 
  * @author Claudenir Fonseca
+ * @author Victor Viola
  *
  */
 public class ViewUtils {
-	
+
 	public static final String SCOPE_PLUGIN = "OntoUML";
 //	public static final String SCOPE_ALL_PLUGINS = "";
 	public static final String SCOPE_VERIFICATION = "Verification Log";
 	public static final String SCOPE_DEVELOPMENT_LOG = "DevLog";
-	
+	public static final String ONTOUML_SIMPLE_LOGO_PATH = "plugins\\ontouml-vp-plugin\\icons\\logo\\ontuml-simple-logo.png";
+
 	public static void log(String message) {
 		ApplicationManager.instance().getViewManager().showMessage(timestamp() + message);
 	}
-	
+
 	public static void log(String message, String scope) {
 		ApplicationManager.instance().getViewManager().showMessage(timestamp() + message, scope);
 	}
-	
+
 	public static void simpleLog(String message) {
 		ApplicationManager.instance().getViewManager().showMessage(message);
 	}
-	
+
 	public static void simpleLog(String message, String scope) {
 		ApplicationManager.instance().getViewManager().showMessage(message, scope);
 	}
-	
+
 	public static void clearLog(String scope) {
 		ApplicationManager.instance().getViewManager().clearMessages(scope);
 	}
-	
+
 	private static String timestamp() {
 		return "[" + (new Timestamp(System.currentTimeMillis())) + "] ";
 	}
-	
+
 	public static void logVerificationResponse(String responseMessage) {
 		try {
 			JsonObject response = (JsonObject) new JsonParser().parse(responseMessage).getAsJsonObject();
 
 			if (response.has("valid") && response.get("valid").getAsBoolean()) {
 				ViewUtils.simpleLog("The model was verified and no syntactical errors were found.\n", SCOPE_PLUGIN);
-			} 
-			else {
+			} else {
 				final JsonArray errors = response.get("meta").getAsJsonArray();
-				
+
 				for (JsonElement elem : errors) {
 					final JsonObject error = elem.getAsJsonObject();
-					final String line = '[' + error.get("title").getAsString() + "]\t " + error.get("detail").getAsString()
-							.replaceAll("ontouml/1.0/", "").replaceAll("ontouml/2.0/", "");
-					
+					final String line = '[' + error.get("title").getAsString() + "]\t " + error.get("detail")
+							.getAsString().replaceAll("ontouml/1.0/", "").replaceAll("ontouml/2.0/", "");
+
 					ViewUtils.simpleLog(line.trim(), SCOPE_PLUGIN);
 				}
 			}
 		} catch (JsonSyntaxException e) {
 			ViewUtils.log("The requested server might be down. See response below:", SCOPE_PLUGIN);
 			ViewUtils.log(responseMessage, SCOPE_PLUGIN);
-			ViewUtils.log("Remote verification error. Please submit your Visual Paradigm's log and the time of the error our developers", SCOPE_PLUGIN);
+			ViewUtils.log(
+					"Remote verification error. Please submit your Visual Paradigm's log and the time of the error our developers",
+					SCOPE_PLUGIN);
 			e.printStackTrace();
 		}
+	}
+
+	public static boolean enableSmartPaintDialog() {
+		final String workspace = ApplicationManager.instance().getWorkspaceLocation().toString(); 
+		final String pathLogo = workspace.substring(0, workspace.length()-2) + ONTOUML_SIMPLE_LOGO_PATH;
+		ImageIcon icon = new ImageIcon(pathLogo, "OntoUML");
+		
+		int isToenable = ApplicationManager.instance().getViewManager().showConfirmDialog(null,
+				"Auto coloring is not enabled. Do you want to activate?", "Smart Paint", JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.INFORMATION_MESSAGE, icon);
+		
+		if(isToenable == 0)
+			return true;
+		else
+			return false;
 	}
 
 }
