9b8b2b0591dd5f729ae252b9adc769613d280fa5
Tiago Prince Sales <tiago.princesales@unibz.it>
2020-04-11 14:28:10 +0200

    Fixed null pointer when canceling the model export

--- src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelExportAction.java
+++ src/main/java/it/unibz/inf/ontouml/vp/controllers/ModelExportAction.java
@@ -5,9 +5,6 @@ import com.vp.plugin.action.VPAction;
 import com.vp.plugin.action.VPActionController;
 import com.vp.plugin.view.IDialog;
 import com.vp.plugin.view.IDialogHandler;
-
-import it.unibz.inf.ontouml.vp.controllers.DiagramVerificationAction.DiagramVerificationRequest;
-import it.unibz.inf.ontouml.vp.controllers.DiagramVerificationAction.ProgressDialog;
 import it.unibz.inf.ontouml.vp.model.ModelElement;
 import it.unibz.inf.ontouml.vp.utils.Configurations;
 import it.unibz.inf.ontouml.vp.utils.ProjectConfigurations;
@@ -18,116 +15,111 @@ import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.concurrent.ExecutorService;
 
 /**
- * 
  * Implementation toolbar button action responsible for exporting OntoUML model
  * in  JSON (according to OntoUML Schema).
- * 
- * @author Claudenir Fonseca
  *
+ * @author Claudenir Fonseca
  */
 public class ModelExportAction implements VPActionController {
 
-	private ProgressPanel progressPanel;
-	private ProgressDialog loading;
-	private IDialog mainDialog;
-	
-	Thread thread;
-	
-	/**
-	 * 
-	 * Performs model export in JSON format.
-	 * 
-	 * @param action
-	 * 
-	 */
-	@Override
-	public void performAction(VPAction action) {
-		final Configurations configs = Configurations.getInstance();
-		final ProjectConfigurations projectConfigurations = configs.getProjectConfigurations();
+   private ProgressPanel progressPanel;
+   private ProgressDialog loading;
+   private IDialog mainDialog;
 
-		FileDialog fd = new FileDialog((Frame) ApplicationManager.instance().getViewManager().getRootFrame(),
-				"Choose destination", FileDialog.SAVE);
+   Thread thread;
 
-		String suggestedFolderPath = projectConfigurations.getExportFolderPath();
-		String suggestedFileName = projectConfigurations.getExportFilename();
+   /**
+    * Performs model export in JSON format.
+    *
+    * @param action
+    */
+   @Override
+   public void performAction(VPAction action) {
+      final Configurations configs = Configurations.getInstance();
+      final ProjectConfigurations projectConfigurations = configs.getProjectConfigurations();
 
-		if(suggestedFileName.isEmpty()){
-			String projectName = ApplicationManager.instance().getProjectManager().getProject().getName();
-			suggestedFileName = projectName+".json";
-		}
+      FileDialog fd = new FileDialog((Frame) ApplicationManager.instance().getViewManager().getRootFrame(),
+              "Choose destination", FileDialog.SAVE);
 
-		fd.setDirectory(suggestedFolderPath);
-		fd.setFile(suggestedFileName);
-		fd.setVisible(true);
+      String suggestedFolderPath = projectConfigurations.getExportFolderPath();
+      String suggestedFileName = projectConfigurations.getExportFilename();
 
-		String fileDirectory = fd.getDirectory();
-		String fileName = fd.getFile();
+      if (suggestedFileName.isEmpty()) {
+         String projectName = ApplicationManager.instance().getProjectManager().getProject().getName();
+         suggestedFileName = projectName + ".json";
+      }
 
-		if(!fileName.endsWith(".json"))
-			fileName+=".json";
+      fd.setDirectory(suggestedFolderPath);
+      fd.setFile(suggestedFileName);
+      fd.setVisible(true);
 
-		
-		
-		if (fileDirectory != null) {
-			loading = new ProgressDialog();
-			ApplicationManager.instance().getViewManager().showDialog(loading);
-			try {
-				
-				final String jsonModel = ModelElement.generateModel(true);
-				Files.write(Paths.get(fileDirectory, fileName), jsonModel.getBytes());
-				projectConfigurations.setExportFolderPath(fileDirectory);
-				projectConfigurations.setExportFilename(fileName);
-				configs.save();
-				ViewUtils.cleanAndShowMessage("Model exported successfuly.");
-			} catch (IOException e) {
-				ViewUtils.cleanAndShowMessage("Export Failed. Please submit your Visual Paradigm's log and the time of the error our developers.");
-				e.printStackTrace();
-			}
-			
-			mainDialog.close();
-		}
-		
-	}
+      String fileDirectory = fd.getDirectory();
 
-	/**
-	 * Called when the menu containing the button is accessed allowing for action
-	 * manipulation, such as enable/disable or selecting the button.
-	 * 
-	 * OBS: DOES NOT apply to this class.
-	 */
-	@Override
-	public void update(VPAction action) {
-	}
-	
-	protected class ProgressDialog implements IDialogHandler {
+      if (fileDirectory != null) {
 
-		@Override
-		public Component getComponent() {
-			progressPanel = new ProgressPanel("Building model...");
-			return progressPanel;
-		}
+         loading = new ProgressDialog();
+         ApplicationManager.instance().getViewManager().showDialog(loading);
 
-		@Override
-		public void prepare(IDialog dialog) {
-			mainDialog = dialog;
-			mainDialog.setTitle("Export to JSON");
-			mainDialog.setModal(false);
-			mainDialog.setResizable(false);
-			dialog.setSize(progressPanel.getWidth(), progressPanel.getHeight() + 20);
-			progressPanel.setContainerDialog(mainDialog);
-		}
+         try {
+            String fileName = fd.getFile();
 
-		@Override
-		public void shown() {
-		}
+            if (!fileName.endsWith(".json"))
+               fileName += ".json";
 
-		@Override
-		public boolean canClosed() {
-			return false;
-		}
+            final String jsonModel = ModelElement.generateModel(true);
+            Files.write(Paths.get(fileDirectory, fileName), jsonModel.getBytes());
+            projectConfigurations.setExportFolderPath(fileDirectory);
+            projectConfigurations.setExportFilename(fileName);
+            configs.save();
+            ViewUtils.cleanAndShowMessage("Model exported successfully.");
+         } catch (IOException e) {
+            ViewUtils.cleanAndShowMessage("Model export failed.");
+            e.printStackTrace();
+         }
 
-	}
+         mainDialog.close();
+      }
+
+   }
+
+   /**
+    * Called when the menu containing the button is accessed allowing for action
+    * manipulation, such as enable/disable or selecting the button.
+    * <p>
+    * OBS: DOES NOT apply to this class.
+    */
+   @Override
+   public void update(VPAction action) {
+   }
+
+   protected class ProgressDialog implements IDialogHandler {
+
+      @Override
+      public Component getComponent() {
+         progressPanel = new ProgressPanel("Building model...");
+         return progressPanel;
+      }
+
+      @Override
+      public void prepare(IDialog dialog) {
+         mainDialog = dialog;
+         mainDialog.setTitle("Export to JSON");
+         mainDialog.setModal(false);
+         mainDialog.setResizable(false);
+         dialog.setSize(progressPanel.getWidth(), progressPanel.getHeight() + 20);
+         progressPanel.setContainerDialog(mainDialog);
+      }
+
+      @Override
+      public void shown() {
+      }
+
+      @Override
+      public boolean canClosed() {
+         return false;
+      }
+
+   }
 }
