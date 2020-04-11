95784be8e50f29884dac982f0e05730f85e6ca6d
Tiago Prince Sales <tiago.princesales@unibz.it>
2020-04-11 18:55:44 +0200

    Executing VP with plugin through Maven

---
+++ scripts/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#user=$(whoami)
+#appDir=${appDir:-"/Applications/Visual Paradigm.app/Contents/Resources/app"}
+#pluginDir=${pluginsDir:-"/Users/${user}/Library/Application Support/VisualParadigm/plugins/ontouml-vp-plugin-0.0.2-SNAPSHOT/classes"}
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+        # echo $1 $2 // Optional to see the parameter:value result
+   fi
+
+  shift
+done
+
+gson="$appDir/com/google/code/gson/gson/2.8.5/gson-2.8.5.jar"
+vpLibDir="$appDir/lib/*"
+vpOrmlibDir="$appDir/ormlib/*"
+
+classpath="$pluginDir:$gson:$vpLibDir:$vpOrmlibDir"
+java="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java"
+
+cd "$appDir/bin" || return
+
+echo "$java -classpath ${classpath} RV debug"
+$java -classpath "$classpath" RV debug
