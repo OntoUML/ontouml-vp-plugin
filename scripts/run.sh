ce9bafacf5efdac6954bda7953c82c406928d428
Claudenir M. Fonseca <claudenirmf@gmail.com>
2020-04-14 13:59:58 +0200

    Updates scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -20,9 +20,9 @@ vpLibDir="$appDir/lib/*"
 vpOrmlibDir="$appDir/ormlib/*"
 
 classpath="$pluginDir:$gson:$vpLibDir:$vpOrmlibDir"
-java="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java"
+java=$(which java)
 
 cd "$appDir/bin" || return
 
 echo "$java -classpath ${classpath} RV debug"
-$java -classpath "$classpath" RV debug
+$java -classpath "$classpath" -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=5005,suspend=n RV debug
