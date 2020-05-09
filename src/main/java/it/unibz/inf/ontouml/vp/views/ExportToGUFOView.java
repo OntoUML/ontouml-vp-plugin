1f5b85aa8dcf00a70bd42cf80e2b3f5b2db377cd
Viola <victorviola86@gmail.com>
2020-05-09 23:18:53 +0100

    new layout for export - wip

--- src/main/java/it/unibz/inf/ontouml/vp/views/ExportToGUFOView.java
+++ src/main/java/it/unibz/inf/ontouml/vp/views/ExportToGUFOView.java
@@ -20,7 +20,9 @@ import it.unibz.inf.ontouml.vp.OntoUMLPlugin;
 import it.unibz.inf.ontouml.vp.utils.Configurations;
 import it.unibz.inf.ontouml.vp.utils.ProjectConfigurations;
 import it.unibz.inf.ontouml.vp.utils.ServerRequest;
-
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -37,6 +39,10 @@ public class ExportToGUFOView extends JPanel {
 	private JTextField IRItxt;
 	private JComboBox<String> formatBox;
 	private JComboBox<String> uriFormatBox;
+	private JComboBox<String> inverseBox;
+	private JComboBox<String> objectBox;
+	private JComboBox<String> analysisBox;
+	private JComboBox<String> packagesBox;
 
 	private JTabbedPane tabbedPane;
 	private JCheckBoxTree packageTree;
@@ -45,8 +51,8 @@ public class ExportToGUFOView extends JPanel {
 	JPanel treePanelPackage;
 	JPanel treePanelDiagram;
 
-	private JButton _btnApply;
-	private JButton _btnCancel;
+	private JButton btnExport;
+	private JButton btnCancel;
 
 	private IDialog _dialog;
 
@@ -55,42 +61,180 @@ public class ExportToGUFOView extends JPanel {
 
 	private HashSet<String> elementsPackageTree = new HashSet<String>();
 	private HashSet<String> elementsDiagramTree = new HashSet<String>();
-	
+
 	public ExportToGUFOView(ProjectConfigurations configurations, ServerRequest request) {
-		setSize(new Dimension(600, 515));
-		
-		GridBagLayout gbl_main = new GridBagLayout();
-		gbl_main.columnWidths = new int[] { 0, 0, 0 };
-		gbl_main.rowHeights = new int[] { 10 ,10, 10 };
-		gbl_main.columnWeights = new double[] { 1.0, 1.0, 0.5 };
-		gbl_main.rowWeights = new double[] { 1.0, 1.0, 0.5 };
-		
-		setLayout(gbl_main);
-		
-		JPanel optionsPanel = new JPanel();
-		
+		setSize(new Dimension(600, 540));
+		setLayout(new GridBagLayout());
+
+		JPanel optionsPanelLeft = new JPanel();
+		JPanel optionsPanelRight = new JPanel();
 		JPanel treePanel = new JPanel();
+		JPanel buttonsPanel = new JPanel();
+
+		optionsPanelLeft.setLayout(new GridBagLayout());
+		optionsPanelRight.setLayout(new GridBagLayout());
+		treePanel.setLayout(new GridBagLayout());
+		buttonsPanel.setLayout(new GridBagLayout());
+
+	    optionsPanelLeft.setPreferredSize(new Dimension(280, 100));
+	    optionsPanelRight.setPreferredSize(new Dimension(280,120));
+	    treePanel.setPreferredSize(new Dimension(560,250));
+	    buttonsPanel.setPreferredSize(new Dimension(200,40));
+
+		GridBagConstraints gbc_main = new GridBagConstraints();
+		gbc_main.insets = new Insets(0, 0, 0, 0);
+		gbc_main.anchor = GridBagConstraints.FIRST_LINE_START;
+		gbc_main.weightx = 0.5;
+		gbc_main.weighty = 0.5;
+		gbc_main.gridx = 0;
+		gbc_main.gridy = 0;
+		add(optionsPanelLeft, gbc_main);
+		gbc_main.anchor = GridBagConstraints.FIRST_LINE_END;
+		gbc_main.weightx = 0.5;
+		gbc_main.weighty = 0.5;
+		gbc_main.gridx = 1;
+		gbc_main.gridy = 0;
+		add(optionsPanelRight, gbc_main);
+		gbc_main.anchor = GridBagConstraints.PAGE_START;
+		gbc_main.weightx = 1.0;
+		gbc_main.weighty = 0.5;
+		gbc_main.gridwidth = 4;
+		gbc_main.gridx = 0;
+		gbc_main.gridy = 1;
+		add(treePanel, gbc_main);
+		gbc_main.anchor = GridBagConstraints.LINE_END;
+		gbc_main.weightx = 0.5;
+		gbc_main.weighty = 0.5;
+		gbc_main.gridx = 0;
+		gbc_main.gridy = 2;
+		add(buttonsPanel, gbc_main);
+
+		JLabel iriLabel = new JLabel("Base IRI:");
+		JLabel formatLabel = new JLabel("Format:");
+		JLabel URIformatLabel = new JLabel("URI Format:");
+
+		IRItxt = new JTextField();
+
+		String[] formatStrings = { "Turtle", "N-Triples", "N-Quads" };
+		formatBox = new JComboBox<String>(formatStrings);
+		((JLabel) formatBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		String[] uriFormatBoxString = { "name", "id" };
+		uriFormatBox = new JComboBox<String>(uriFormatBoxString);
+		((JLabel) uriFormatBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		iriLabel.setPreferredSize(new Dimension(80, 20));
+		formatLabel.setPreferredSize(new Dimension(80, 20));
+		URIformatLabel.setPreferredSize(new Dimension(80, 20));
+		IRItxt.setPreferredSize(new Dimension(120, 20));
+		formatBox.setPreferredSize(new Dimension(80, 20));
+		uriFormatBox.setPreferredSize(new Dimension(80, 20));
+
+		GridBagConstraints gbc_insidePanelLeft = new GridBagConstraints();
+		// gbc_insidePanelLeft.fill = GridBagConstraints.HORIZONTAL;
+		gbc_insidePanelLeft.insets = new Insets(5, 5, 5, 5);
+		gbc_insidePanelLeft.weightx = 0.5;
+		gbc_insidePanelLeft.gridx = 0;
+		gbc_insidePanelLeft.gridy = 0;
+		gbc_insidePanelLeft.anchor = GridBagConstraints.WEST;
+		optionsPanelLeft.add(iriLabel, gbc_insidePanelLeft);
+		gbc_insidePanelLeft.weightx = 0.5;
+		gbc_insidePanelLeft.gridx = 1;
+		gbc_insidePanelLeft.gridy = 0;
+		optionsPanelLeft.add(IRItxt, gbc_insidePanelLeft);
+		gbc_insidePanelLeft.weightx = 0.5;
+		gbc_insidePanelLeft.gridx = 0;
+		gbc_insidePanelLeft.gridy = 1;
+		optionsPanelLeft.add(formatLabel, gbc_insidePanelLeft);
+		gbc_insidePanelLeft.weightx = 0.5;
+		gbc_insidePanelLeft.gridx = 1;
+		gbc_insidePanelLeft.gridy = 1;
+		optionsPanelLeft.add(formatBox, gbc_insidePanelLeft);
+		gbc_insidePanelLeft.weightx = 0.5;
+		gbc_insidePanelLeft.gridx = 0;
+		gbc_insidePanelLeft.gridy = 2;
+		optionsPanelLeft.add(URIformatLabel, gbc_insidePanelLeft);
+		gbc_insidePanelLeft.weightx = 0.5;
+		gbc_insidePanelLeft.gridx = 1;
+		gbc_insidePanelLeft.gridy = 2;
+		optionsPanelLeft.add(uriFormatBox, gbc_insidePanelLeft);
+
+		JLabel inverseLabel = new JLabel("Inverse:");
+		JLabel objectLabel = new JLabel("Object Property:");
+		JLabel analysisLabel = new JLabel("Pre Analysis:");
+		JLabel prefixPackageLabel = new JLabel("Prefix Package:");
+
+		String[] inverseBoxString = { "false", "true" };
+		inverseBox = new JComboBox<String>(inverseBoxString);
+		((JLabel) inverseBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		String[] objectBoxString = { "false", "true" };
+		objectBox = new JComboBox<String>(objectBoxString);
+		((JLabel) objectBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		String[] analysisBoxString = { "true", "false" };
+		analysisBox = new JComboBox<String>(analysisBoxString);
+		((JLabel) analysisBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		String[] packagesBoxString = { "false", "true" };
+		packagesBox = new JComboBox<String>(packagesBoxString);
+		((JLabel) packagesBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
+
+		inverseLabel.setPreferredSize(new Dimension(80, 20));
+		objectLabel.setPreferredSize(new Dimension(80, 20));
+		analysisLabel.setPreferredSize(new Dimension(80, 20));
+		prefixPackageLabel.setPreferredSize(new Dimension(80, 20));
+		inverseBox.setPreferredSize(new Dimension(80, 20));
+		objectBox.setPreferredSize(new Dimension(80, 20));
+		analysisBox.setPreferredSize(new Dimension(80, 20));
+		packagesBox.setPreferredSize(new Dimension(80, 20));
+
+		GridBagConstraints gbc_insidePanelRight = new GridBagConstraints();
+		// gbc_insidePanelLeft.fill = GridBagConstraints.HORIZONTAL;
+		gbc_insidePanelRight.insets = new Insets(5, 5, 5, 5);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 0;
+		gbc_insidePanelRight.gridy = 0;
+		gbc_insidePanelRight.anchor = GridBagConstraints.WEST;
+		optionsPanelRight.add(inverseLabel, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 1;
+		gbc_insidePanelRight.gridy = 0;
+		optionsPanelRight.add(inverseBox, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 0;
+		gbc_insidePanelRight.gridy = 1;
+		optionsPanelRight.add(objectLabel, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 1;
+		gbc_insidePanelRight.gridy = 1;
+		optionsPanelRight.add(objectBox, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 0;
+		gbc_insidePanelRight.gridy = 2;
+		optionsPanelRight.add(analysisLabel, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 1;
+		gbc_insidePanelRight.gridy = 2;
+		optionsPanelRight.add(analysisBox, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 0;
+		gbc_insidePanelRight.gridy = 3;
+		optionsPanelRight.add(prefixPackageLabel, gbc_insidePanelRight);
+		gbc_insidePanelRight.weightx = 0.5;
+		gbc_insidePanelRight.gridx = 1;
+		gbc_insidePanelRight.gridy = 3;
+		optionsPanelRight.add(packagesBox, gbc_insidePanelRight);
 		
 		JPanel packagePanel = new JPanel();
 		JPanel diagramPanel = new JPanel();
 		
 		tabbedPane = new JTabbedPane();
-		
 		packageTree = new JCheckBoxTree("package");		
 		diagramTree = new JCheckBoxTree("diagram");
 		
 		JScrollPane scrollableTextAreaPackage = new JScrollPane(packageTree);
 		JScrollPane scrollableTextAreaDiagram = new JScrollPane(diagramTree);
-		
-		IRItxt = new JTextField();
-		
-		String[] formatStrings = { "N-Triples", "N-Quads", "Turtle" };
-		formatBox = new JComboBox<String>(formatStrings);
-		((JLabel) formatBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
-		
-		String[] uriFormatBoxString = { "name", "id" };
-		uriFormatBox = new JComboBox<String>(uriFormatBoxString);
-		((JLabel) uriFormatBox.getRenderer()).setHorizontalAlignment(JLabel.CENTER);
 		
 		scrollableTextAreaPackage.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scrollableTextAreaPackage.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -99,10 +243,10 @@ public class ExportToGUFOView extends JPanel {
 		scrollableTextAreaDiagram.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		packagePanel.setLayout(new GridLayout(1, 1));
-		packagePanel.setPreferredSize(new Dimension(550, 300));
+		packagePanel.setPreferredSize(new Dimension(550, 270));
 		
 		diagramPanel.setLayout(new GridLayout(1, 1));
-		diagramPanel.setPreferredSize(new Dimension(550, 300));
+		diagramPanel.setPreferredSize(new Dimension(550, 270));
 		
 		packagePanel.add(scrollableTextAreaPackage);
 		
@@ -119,43 +263,14 @@ public class ExportToGUFOView extends JPanel {
 		treePanel.add(tabbedPane);
 		treePanel.setLayout(new GridLayout(1,1));
 		
-		GridLayout gl_optionsPanel = new GridLayout(3,2);
-		gl_optionsPanel.setVgap(5);
-		gl_optionsPanel.setHgap(5);
-		optionsPanel.setLayout(gl_optionsPanel);
-	
-		optionsPanel.add(new JLabel("Base IRI:"));
-		optionsPanel.add(IRItxt);
 		
-		optionsPanel.add(new JLabel("Format:"));
-		optionsPanel.add(formatBox);
-		optionsPanel.add(new JLabel("URI Format:"));
-		optionsPanel.add(uriFormatBox);
+		btnExport = new JButton("Export");
+		btnCancel = new JButton("Cancel");
 		
-		GridBagConstraints gbc_optionsPanel = new GridBagConstraints();
-		gbc_optionsPanel.insets = new Insets(5, 5, 5, 0);
-		gbc_optionsPanel.anchor = GridBagConstraints.WEST;
-		gbc_optionsPanel.gridx = 0;
-		gbc_optionsPanel.gridy = 0;
-		gbc_optionsPanel.ipadx = 30;
+		btnExport.setPreferredSize(new Dimension(80, 35));
+		btnCancel.setPreferredSize(new Dimension(80, 35));
 		
-		add(optionsPanel, gbc_optionsPanel);
-		
-		GridBagConstraints gbc_treePanel = new GridBagConstraints();
-		gbc_optionsPanel.insets = new Insets(5, 5, 5, 0);
-		gbc_treePanel.gridx = 0;
-		gbc_treePanel.gridy = 1;
-		add(treePanel, gbc_treePanel);
-		
-		JPanel controlButtonsPanel = new JPanel();
-		
-		GridLayout glcontrolButtonsPanel = new GridLayout(1,2);
-		gl_optionsPanel.setVgap(5);
-		gl_optionsPanel.setHgap(5);
-		controlButtonsPanel.setLayout(glcontrolButtonsPanel);
-
-		_btnApply = new JButton("Export");
-		_btnApply.addActionListener(new ActionListener() {
+		btnExport.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
@@ -173,10 +288,8 @@ public class ExportToGUFOView extends JPanel {
 				thread.start();
 			}
 
-		});
-
-		_btnCancel = new JButton("Cancel");
-		_btnCancel.addActionListener(new ActionListener() {
+		});		
+		btnCancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				isToExport = false;
@@ -188,20 +301,20 @@ public class ExportToGUFOView extends JPanel {
 
 		});
 		
-		controlButtonsPanel.add(_btnApply);
-		controlButtonsPanel.add(_btnCancel);
+		GridBagConstraints gbc_buttonsPanel = new GridBagConstraints();
+		gbc_buttonsPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_buttonsPanel.insets = new Insets(2, 2, 2, 2);
+		gbc_buttonsPanel.weightx = 0.5;
+		gbc_buttonsPanel.gridx = 0;
+		gbc_buttonsPanel.gridy = 0;
+		gbc_buttonsPanel.anchor = GridBagConstraints.FIRST_LINE_END;
+		buttonsPanel.add(btnExport, gbc_buttonsPanel);
+		gbc_buttonsPanel.weightx = 0.5;
+		gbc_buttonsPanel.gridx = 1;
+		gbc_buttonsPanel.gridy = 0;
+		buttonsPanel.add(btnCancel, gbc_buttonsPanel);
 		
-		GridBagConstraints gbc_controlButtonsPanel = new GridBagConstraints();
-		gbc_controlButtonsPanel.anchor = GridBagConstraints.SOUTHEAST;
-		gbc_controlButtonsPanel.insets = new Insets(5, 5, 5, 0);
-		gbc_controlButtonsPanel.gridx = 0;
-		gbc_controlButtonsPanel.gridy = 2;
-		gbc_controlButtonsPanel.ipadx = 130;
-		add(controlButtonsPanel, gbc_controlButtonsPanel);
-		
-		isToExport = false;
-		isOpen = true;
-		updateComponentsValues(configurations);
+
 	}
 
 	protected JComponent makeTextPanel(String text) {
@@ -298,7 +411,7 @@ public class ExportToGUFOView extends JPanel {
 
 					if (node.getUserObject() instanceof IModelElement)
 						aux.add(((IModelElement) node.getUserObject()).getId());
-					
+
 					if (node.getUserObject() instanceof IDiagramUIModel)
 						aux.add(((IDiagramUIModel) node.getUserObject()).getId());
 				}
