9e3b3f04fcfea2a192a2b6ee1143a1e8248fc61c
Claudenir Fonseca <claudenirmf@gmail.com>
2020-08-20 20:08:59 +0200

    Initial commit
    Changes alpha version

--- src/main/java/it/unibz/inf/ontouml/vp/OntoUMLPlugin.java
+++ src/main/java/it/unibz/inf/ontouml/vp/OntoUMLPlugin.java
@@ -19,7 +19,7 @@ import it.unibz.inf.ontouml.vp.listeners.ProjectListener;
  */
 public class OntoUMLPlugin implements VPPlugin {
 
-   public static final String PLUGIN_VERSION_RELEASE = "0.4.0alpha1";
+   public static final String PLUGIN_VERSION_RELEASE = "0.4.0alpha2";
    public static final String PLUGIN_ID = "it.unibz.inf.ontouml.vp";
    public static final String PLUGIN_NAME = "OntoUML Plugin";
    public static final String PLUGIN_REPO = "https://github.com/OntoUML/ontouml-vp-plugin/";
